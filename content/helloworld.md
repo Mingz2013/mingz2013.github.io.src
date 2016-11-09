ebb92499ea409373ad0b1771fd743d8821a67af7
mingz <305603665@qq.com>
2016-11-09 18:35:59 +0800

    123

--- content/helloworld.md
+++ content/helloworld.md
@@ -3,8 +3,8 @@ Date: 2010-12-03 10:20
 Modified: 2010-12-05 19:30
 Category: Python
 Tags: pelican, publishing
-Slug: my-super-post-2
-Author: Alexis Metaireau
+Slug: helloworld
+Author: Mingz
 Summary: Short version for index and feeds
 
 This is the content of my super blog post.
--- pelicanconf.py
+++ pelicanconf.py
@@ -3,6 +3,24 @@
 from __future__ import unicode_literals
 
 AUTHOR = u'mingz'  # 默认作者
+DATE_FORMATS = {} # 日期格式设置
+USE_FOLDER_AS_CATEGORY = True # 用目录作为类别
+DEFAULT_CATEGORY = 'misc' # 默认文章分类
+DEFAULT_DATE_FORMAT = '%a %d %B %Y' # 默认日期格式
+DISPLAY_PAGES_ON_MENU = True # 是否在模板菜单上显示页面
+DISPLAY_CATEGORIES_ON_MENU = True # 是否在模板菜单上显示分类
+DEFAULT_DATE = "fs" # (2012, 3, 2, 14, 1, 1) 默认日期
+# global metadata to all the contents
+DEFAULT_METADATA = {'yeah': 'it is'} # 文章和页面的默认元数据设置
+# FILENAME_METADATA = '(?P<date>\d{4}-\d{2}-\d{2}).*'
+DELETE_OUTPUT_DIRECTORY = False # 删除putput目录，优点在于避免生成不必要的文件，同时， 该设置具有一定风险，请谨慎处理。
+JINJA_EXTENSIONS = [] # 使用Jinja扩展列表
+JINJA_FILTERS = {} # Jinja2 filters自定义列表
+
+OUTPUT_PATH = 'output/' # 文件生成目录
+PATH = 'content' # 输入文件目录
+
+
 SITENAME = u"mingz's home"
 SITEURL = 'http://localhost'
 TIMEZONE = 'Europe/Paris'
@@ -10,7 +28,7 @@ TIMEZONE = 'Europe/Paris'
 # can be useful in development, but set to False when you're ready to publish
 RELATIVE_URLS = True
 
-PATH = 'content'
+
 
 THEME = "themes/mycustomtheme"  # 主题
 GITHUB_URL = 'http://github.com/mingz2013/'
@@ -19,12 +37,12 @@ PDF_GENERATOR = False
 REVERSE_CATEGORY_ORDER = True
 LOCALE = "C"
 DEFAULT_PAGINATION = 10  # 分页长度
-DEFAULT_DATE = (2012, 3, 2, 14, 1, 1)
+
 
 FEED_ALL_RSS = 'feeds/all.rss.xml'
 CATEGORY_FEED_RSS = 'feeds/%s.rss.xml'
 
-USE_FOLDER_AS_CATEGORY = True
+
 
 DEFAULT_LANG = u'en'
 
@@ -46,8 +64,6 @@ SOCIAL = (('twitter', 'http://twitter.com/ametaireau'),
           ('lastfm', 'http://lastfm.com/user/akounet'),
           ('github', 'http://github.com/ametaireau'),)
 
-# global metadata to all the contents
-DEFAULT_METADATA = {'yeah': 'it is'}
 
 # static paths will be copied under the same name
 # static paths will be copied without parsing their contents
